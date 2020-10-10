64b3a964b6054c5d79a7dac5731bc8de8dbf92ec
RoshanKajulkar <roshankajulkar@gmail.com>
2020-10-10 18:17:44 +0530

    user auth and product api's created

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 8000
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//My routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const productRoutes = require("./routes/product");
+
+//DB Connection 
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/auth", authRoutes);
+app.use("/user",userRoutes);
+app.use("/product",productRoutes);
+
+//PORT
+const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello !')
 })
 
+//Starting a server
 app.listen(port, () => {
-  console.log(`server running at http://localhost:${port}`)
-})
+  console.log(`app is running at ${port}`);
+});
+   
